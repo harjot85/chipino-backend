15d62eb4dc6a88272efd4acc0e5419df496ccaa9
Harjot <singh.harjot85@gmail.com>
2019-06-16 18:20:21 -0700

    add controller and model to recieve data from GitHub webhook

---
+++ Controllers/UpdateGitRepoCollectionController.cs
@@ -0,0 +1,45 @@
+﻿using backend_website.Models;
+using backend_website.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_website.Controllers
+{
+    [Route("/api/[controller]")]
+    [EnableCors("ChipinoOriginPolicy")]
+    [ApiController]
+    public class UpdateGitRepoCollectionController : Controller
+    {
+        private readonly MongoDbService _service;
+
+        private const string Added = "created";
+        private const string Removed = "deleted";
+
+        public UpdateGitRepoCollectionController(MongoDbService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public IActionResult UpdateGitRepoCollection([FromBody] dynamic repository)
+        {
+            string result = string.Empty;
+            var repo = new GitHubRepository
+            {
+                Action = repository.action,
+                Id = repository.repository.id,
+                Name = repository.repository.name,
+                Description = repository.repository.description
+            };
+
+            if (!string.IsNullOrEmpty(repo.Action))
+            {
+                if (repo.Action == Added)
+                { }
+                if (repo.Action == Removed)
+                { }
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ Models/GitHubRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace backend_website.Models
+{
+    public class GitHubRepository
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Action { get; set; }
+    }
+}
